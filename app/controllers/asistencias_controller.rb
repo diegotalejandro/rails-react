02b1703c4d3eac8154dd4c271f25c61d1fadc2ae
diegotalejandro <diego.torreblanca2013@gmail.com>
2019-06-09 11:48:20 -0400

    0.0.1

---
+++ app/controllers/asistencias_controller.rb
@@ -0,0 +1,79 @@
+class AsistenciasController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :set_asistencium, only: [:show, :edit, :update, :destroy]
+
+  # GET /asistencia
+  # GET /asistencia.json
+  def index
+    @asistencias = Asistencium.all
+  end
+
+  # GET /asistencia/1
+  # GET /asistencia/1.json
+  def show
+  end
+
+  # GET /asistencia/new
+  def new
+    @asistencium = Asistencium.new
+  end
+
+  # GET /asistencia/1/edit
+  def edit
+    @asistencium = Asistencium.find params[:id]
+  end
+
+  # POST /asistencia
+  # POST /asistencia.json
+  def create
+    @success=false
+    @asistencium = Asistencium.new(asistencium_params)
+      if @asistencium.valid?
+      @asistencium.save
+      @success=true
+      redirect_to asistencias_path, notice: "Asistencia creado exitosamente"
+    else
+      render :new
+      end
+
+  end
+
+  # PATCH/PUT /asistencia/1
+  # PATCH/PUT /asistencia/1.json
+  def update
+    @success=true
+    if Asistencium.exists?(params[:id])
+      @asistencium = Asistencium.find params[:id]
+      if @asistencium.update(asistencium_params)
+
+        redirect_to asistencias_path, notice: "asistencium editado exitosamente"
+      else
+        flash.now[:alert] = "Ha ocurrido un error"
+        render :edit
+      end
+    else
+      redirect_to asistencias_path, alert: "asistencium no existe"
+    end
+  end
+
+  # DELETE /asistencia/1
+  # DELETE /asistencia/1.json
+  def destroy
+    @asistencium.destroy
+    respond_to do |format|
+      format.html { redirect_to asistencias_path, notice: 'Asistencium was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_asistencium
+      @asistencium = Asistencium.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def asistencium_params
+      params.require(:asistencium).permit(:nombre, :apellido, :correo, :telefono, :institucion)
+    end
+end
---
+++ app/views/asistencias/edit.json.jbuilder
@@ -0,0 +1,6 @@
+json.id @asistencium[:id]
+json.nombre @asistencium[:nombre]
+json.apellido @asistencium[:apellido]
+json.correo @asistencium[:correo]
+json.telefono @asistencium[:telefono]
+json.institucion @asistencium[:institucion]
